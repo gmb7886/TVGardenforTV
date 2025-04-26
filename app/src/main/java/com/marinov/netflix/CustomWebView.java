1b49e5acfb1e9aac250c227728f8e668e83d1a99
Gabriel MB <gabrielbaraglio@outlook.com.br>
2025-04-26 19:47:17 -0300

    Primeiro commit

---
+++ app/src/main/java/com/marinov/netflix/CustomWebView.java
@@ -0,0 +1,32 @@
+package com.marinov.netflix;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.webkit.WebView;
+
+public class CustomWebView extends WebView {
+    public CustomWebView(Context context) {
+        super(context);
+    }
+
+    public CustomWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onCheckIsTextEditor() {
+        // Habilita edição de texto
+        return true;
+    }
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        // Configura IME para texto simples
+        InputConnection ic = super.onCreateInputConnection(outAttrs);
+        outAttrs.inputType = EditorInfo.TYPE_CLASS_TEXT;
+        outAttrs.imeOptions = EditorInfo.IME_FLAG_NO_EXTRACT_UI;
+        return ic;
+    }
+}
